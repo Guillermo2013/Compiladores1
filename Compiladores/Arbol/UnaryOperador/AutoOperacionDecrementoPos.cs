069198a724fc9f9d5ea7a52a8168dd5d14cd48bd
GUILLERMO JOSE SANDOVAL <memojose_02@hotmail.com>
2016-11-18 02:23:08 -0600

    arbol commit 1
    create  all classes

---
+++ Compiladores/Arbol/UnaryOperador/AutoOperacionDecrementoPos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Compiladores.Arbol;
+namespace Compiladores.Arbol.UnaryOperador
+{
+    public class AutoOperacionDecrementoPos : UnaryOperadorNode
+    {
+       
+    }
+}
---
+++ Compiladores/Arbol/UnaryOperadorNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiladores.Arbol.UnaryOperador
+{
+    public abstract class UnaryOperadorNode : ExpressionNode
+    {
+       public string Value;
+       public ExpressionNode Operando;
+    }
+}
